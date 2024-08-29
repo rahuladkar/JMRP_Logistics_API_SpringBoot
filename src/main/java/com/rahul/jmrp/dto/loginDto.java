b366e3a334d568d1be73db6e8cc19aa75e93be72
Rahul Adkar <90685828+rahuladkar@users.noreply.github.com>
2024-08-29 12:50:03 +0530

    Add files via upload

---
+++ src/main/java/com/rahul/jmrp/dto/loginDto.java
@@ -0,0 +1,35 @@
+package com.rahul.jmrp.dto;
+
+public class loginDto {
+
+	private String username;
+	private String password;
+	private String userRole;
+
+	// getter() and setter()
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+}
